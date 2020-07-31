be0606b07aa709538bfb52850e6b0c63c0103bb9
Andrei Aquino <andreimartinbaquino@gmail.com>
2020-07-31 23:41:53 +0800

    refactored and added button with function to fetch

--- nodeStuff/index.js
+++ nodeStuff/index.js
@@ -9,3 +9,19 @@ const app = express();
 app.listen(3000, () => console.log('listening at port 3000'));
 
 app.use(express.static('public'));
+app.use(express.json({limit: '1mb'})); //for express to understand incoming json data
+
+const coords = [];
+
+app.post('/api', (req, res) => {
+  console.log('i got a request');
+  console.log(req.body);
+  const data = req.body;
+  coords.push(data)
+  res.json({
+    status: 'Sucess',
+    latitude: data.lat,
+    longitude: data.long
+  });
+  console.log(coords);
+});
