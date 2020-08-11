dfdf5cb2183f1fe83596f1e5b6f33b95c6619240
Andrei Aquino <andreimartinbaquino@gmail.com>
2020-08-11 22:45:35 +0800

    added .env file to hide api key

--- nodeApi/index.js
+++ nodeApi/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const Datastore = require('nedb');
 const fetch = require('node-fetch');
+require('dotenv').config();
+
 const cors = require('cors');
 const app = express();
 
@@ -10,7 +12,7 @@ app.listen(3001, () => {
 
 app.use(express.static('public')); // serve static file inside public
 app.use(express.json({ limit: '1mb'})); // understand incoming json with limit
-app.use(cors());
+// app.use(cors());
 
 const database = new Datastore('databaseCheckin.db');
 database.loadDatabase();
@@ -44,7 +46,7 @@ app.get('/api/weather/:latlong', cors(),  async (request, response) => {
   const long = latlong[1];
   
   //wheather api
-  const api_key = 'EZ4KKdhEMSvs6Ukd7q4rpnDevByvdYVl';
+  const api_key = process.env.API_KEY;
   const weather_api_url = `https://api.climacell.co/v3/weather/nowcast?lat=${lat}&lon=${long}&timestep=5&unit_system=si&fields=precipitation_type,temp%3AC,feels_like%3AC,dewpoint%3AC,wind_speed%3Akph,wind_gust%3Akph,baro_pressure%3AmmHg,visibility%3Akm,humidity%3A%25,wind_direction%3Adegrees,sunrise,sunset,cloud_cover%3A%25,cloud_ceiling%3Aft,cloud_base%3Aft,weather_code&start_time=now&apikey=${api_key}`;
   const weatheFetchResponse = await fetch(weather_api_url);
   const weatherResponseJson = await weatheFetchResponse.json();
